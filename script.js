8335c96b9fa529349ce4ec6febb6071996b40d44
HoaVu <vtthoapl@gmail.com>
2024-01-17 18:33:01 +0200

    My profolio

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function toggleMenu() {
-    const icon = document.querySelector(".hamburger-icon")
-    const menu = document.querySelector(".menu-links")
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
-}
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    if (menu && icon) {
+      menu.classList.toggle("open");
+      icon.classList.toggle("open");
+    } else {
+      console.error("Menu or icon not found");
+    }
+  }
+  
